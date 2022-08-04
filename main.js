27a10af9202ba5a22fefd52f29510fc4406cbe0a
jesusLayton <jesuslayton92@gmail.com>
2022-08-03 20:10:16 -0500

    actualizacion

---
+++ main.js
@@ -0,0 +1,11 @@
+const task =[];
+let time = 0;
+let timer= null;
+let timerBreak = null;
+let current =null;
+
+const bAdd = document.querySelectorAll('#bAdd');
+const itTask = document.querySelectorAll('#itTask');
+const form= document.querySelectorAll('#form');
+form.addEventListener
+
